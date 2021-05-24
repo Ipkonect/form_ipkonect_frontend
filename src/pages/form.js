1de1ce9d2d4f85bda2c225fd16c0da32a5d93526
Ipkonect <75092008+Ipkonect@users.noreply.github.com>
2021-05-24 10:58:18 -0300

    Update form.js

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,7 +1,5 @@
 import React, { useState, useRef } from 'react';
 
-import ReCAPTCHA from 'react-google-recaptcha';
-
 import InputMask from 'react-input-mask';
 
 import api from '../services/api';
@@ -16,15 +14,10 @@ function FORM({ history }) {
     const [name, set_name] = useState('');
     const [number, set_number] = useState('');
     const [error, set_error] = useState('');
-    const [captcha, set_captcha] = useState('');
 
     const email_ref = useRef(null);
     const name_ref = useRef(null);
     const number_ref = useRef(null);
-
-    function onChange(value) {
-        set_captcha(value)
-    }
 
     async function handle_register(e) {
         e.preventDefault();
@@ -34,8 +27,6 @@ function FORM({ history }) {
         email_ref.current.style.border = 'none';
         name_ref.current.style.border = 'none';
         number_ref.current.style.border = 'none';
-
-        
 
         if(email_ref.current.value === ''){
             email_ref.current.style.border = '1px solid #FF0000';
@@ -86,11 +77,6 @@ function FORM({ history }) {
                     <input ref={name_ref} className="input_name" placeholder="Seu nome" onChange={e => set_name(e.target.value)} />
                     <InputMask ref={number_ref} mask="(99) 9999-9999" className="input_phone" placeholder="Seu número" onChange={e => set_number(e.target.value)} />;
                     <p className="error">{error}</p>
-                    <ReCAPTCHA
-                        className="recaptcha"
-                        sitekey="6Ld2IF8aAAAAAJynBYXa8rixjsn4Y0atwNlA2grL"
-                        onChange={onChange}
-                    />
                     <p className="politic_and_terms_user">Ao se registrar, você aceita nossos <a href="https://ipkonect.com.br/user_terms.html" target="_blank">termos de uso</a> e a nossa <a href="https://ipkonect.com.br/privacity_politic.html" target="_blank">política de privacidade</a>.</p>
                     <button className="button_register" onClick={handle_register}>CADASTRAR</button>
                 </form>
